2539a2d7a6751be7c1ea9383865d1f15946ef9f1
patricio-jp <patitojimenez1610@hotmail.com>
2021-06-11 02:32:11 -0300

    Modificación página Inicio (incluye CSS), eliminación de archivos temporales de phpMyAdmin

--- htdocs/muspi_merol/wp-content/themes/neve-hijo/functions.php
+++ htdocs/muspi_merol/wp-content/themes/neve-hijo/functions.php
@@ -14,4 +14,22 @@ if ( get_stylesheet() !== get_template() ) {
         return get_option( 'theme_mods_' . get_template(), $default );
     } );
 }
+/**
+ * Font Awesome Kit Setup
+ * 
+ * This will add your Font Awesome Kit to the front-end, the admin back-end,
+ * and the login screen area.
+ */
+ if (! function_exists('fa_custom_setup_kit') ) {
+    function fa_custom_setup_kit($kit_url = 'https://kit.fontawesome.com/318bbb0cc7.js') {
+      foreach ( [ 'wp_enqueue_scripts', 'admin_enqueue_scripts', 'login_enqueue_scripts' ] as $action ) {
+        add_action(
+          $action,
+          function () use ( $kit_url ) {
+            wp_enqueue_script( 'font-awesome-kit', $kit_url, [], null );
+          }
+        );
+      }
+    }
+  }
 ?>
