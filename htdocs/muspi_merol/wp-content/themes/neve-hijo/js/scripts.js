282bdd6746d710ae2b78f2c8e9ba703b02d11f99
patricio-jp <patitojimenez1610@hotmail.com>
2021-07-03 17:18:04 -0300

    Solucionado nav responsive de Elementor, actualizado plugin de Headers Elementor

--- htdocs/muspi_merol/wp-content/themes/neve-hijo/js/scripts.js
+++ htdocs/muspi_merol/wp-content/themes/neve-hijo/js/scripts.js
@@ -63,4 +63,27 @@ if(ulnav === null) {
     spanIcono.appendChild(icono);
     spanIcono2.appendChild(icono2);
 
+
+    //
+    // Nav Responsive igual al del tema original
+    //
+    // 1. Creación del elemento toggle Abrir Menú
+    const divButton = document.createElement("div");
+    divButton.className = "header--row header-top hide-on-desktop layout-full-contained header--row";
+    divButton.dataset.rowId = "top";
+    divButton.dataset.showOn = "mobile";
+    divButton.innerHTML = '<div class="header--row-inner header-top-inner"><div class="container"><div class="row row--wrapper" data-section="hfg_header_layout_top"><div class="builder-item hfg-item-last hfg-item-first col-4 tablet-right mobile-right offset-8"><div class="item--inner builder-item--nav-icon" data-section="header_menu_icon" data-item-id="nav-icon"><div class="menu-mobile-toggle item-button navbar-toggle-wrapper"><button class="navbar-toggle" aria-label="Menú de navegación"><div class="bars"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></div><span class="screen-reader-text">Alternar la navegación</span></button></div></div></div></div></div></div>';
+    // 2. Creación del elemento que contiene al menú que se desplaza desde la derecha
+    const divMenu = document.createElement("div");
+    divMenu.id = "header-menu-sidebar";
+    divMenu.className = "header-menu-sidebar menu-sidebar-panel slide_right";
+    divMenu.innerHTML = '<div id="header-menu-sidebar-bg" class="header-menu-sidebar-bg"><div class="close-sidebar-panel navbar-toggle-wrapper"><button class="navbar-toggle active" aria-label="Menú de navegación"><div class="bars"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></div><span class="screen-reader-text">Alternar la navegación</span></button></div><div id="header-menu-sidebar-inner" class="header-menu-sidebar-inner "><div class="builder-item has-nav hfg-item-last hfg-item-first col-12 mobile-center tablet-center desktop-left"><div class="item--inner builder-item--primary-menu has_menu" data-section="header_menu_primary" data-item-id="primary-menu"><div class="nv-nav-wrap"><div role="navigation" class="style-plain nav-menu-primary" aria-label="Menú principal"><ul id="nv-primary-navigation-sidebar" class="primary-menu-ul nav-ul"><li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home menu-item-231"><a href="http://localhost/muspi_merol/">Inicio</a></li><li class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-229 current_page_item current_page_parent menu-item-232"><a href="http://localhost/muspi_merol/novedades/" aria-current="page">Novedades</a></li><li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-254"><a href="http://localhost/muspi_merol/mision-wichi/"><span class="menu-item-title-wrap">Misión Wichí</span><div class="caret-wrap 3" tabindex="0"><span class="caret"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z"></path></svg></span></div></a><ul class="sub-menu" style="right: 100%; left: auto;"><li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-255"><a href="http://localhost/muspi_merol/mision-wichi/donaciones/">Donaciones</a></li><li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-501"><a href="http://localhost/muspi_merol/mision-wichi/contacto/">Contacto</a></li></ul></li><li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-256"><a href="http://localhost/muspi_merol/hogar-san-jose/"><span class="menu-item-title-wrap">Hogar de día</span><div class="caret-wrap 6" tabindex="0"><span class="caret"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z"></path></svg></span></div></a><ul class="sub-menu" style="right: 100%; left: auto;"><li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-273"><a href="https://donaronline.org/hogar-de-dia-san-jose/ayuda-a-sostener-nuestros-talleres-educativos-para-ninos-y-adolescentes?preview=true">Donaciones</a></li><li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-238"><a href="http://localhost/muspi_merol/hogar-san-jose/contacto/">Contacto</a></li></ul></li></ul</div></div></div></div></div></div>';
+    // 3. Creación del elemento "overlay"
+    const divOverlay = document.createElement("div");
+    divOverlay.className = "header-menu-sidebar-overlay";
+    // 2. Seleccionar el header donde será incluido el elemento creado y añadirlo como primer hijo
+    let divHeader = document.querySelector("#masthead > div > div.elementor-section-wrap");
+    divHeader.insertAdjacentElement("afterbegin", divButton);
+    divHeader.appendChild(divMenu); 
+    divHeader.appendChild(divOverlay); 
 }
